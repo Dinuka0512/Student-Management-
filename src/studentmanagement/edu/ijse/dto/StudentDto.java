57cd4c3707af46fa2279de67588bcbcbe45d4f66
Dinuka0512 <Dinuka0512@gmail.com>
2024-09-14 01:02:52 +0530

    Data Transfer Object Class created

--- src/studentmanagement/edu/ijse/dto/StudentDto.java
+++ src/studentmanagement/edu/ijse/dto/StudentDto.java
@@ -9,5 +9,55 @@ package studentmanagement.edu.ijse.dto;
  * @author dinuka
  */
 public class StudentDto {
+    private String studentID;
+    private String name;
+    private String address;
+    private int grade;
     
+    public StudentDto() {
+    }
+
+    public StudentDto(String studentID, String name, String address, int grade) {
+        this.studentID = studentID;
+        this.name = name;
+        this.address = address;
+        this.grade = grade;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentDto{" + "studentID=" + studentID + ", name=" + name + ", address=" + address + ", grade=" + grade + '}';
+    }
 }
